78cb617fdca7efe711168c1198fa96347e55d80d
noddy0070 <utkarshsaxena@rediffmail.com>
2025-09-26 02:06:39 +0530

    changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Hero from "./hero";
+import Hero from "./Hero";
 import Feature from "./feature";
 import CareerCounselling from "./CareerCounselling";
 import Visa from "./Visa";
