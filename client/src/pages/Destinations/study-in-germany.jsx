c588690101c0e9986d5783b3a75daf09def55c9c
noddy0070 <utkarshsaxena@rediffmail.com>
2025-12-02 11:10:45 +0530

    can't download img

--- client/src/pages/Destinations/study-in-germany.jsx
+++ client/src/pages/Destinations/study-in-germany.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import studyInGermanyHeaderImg from '../../assets/images/germanyPage.png'
-
+import { useEffect } from 'react';
 const StudyInGermany = () => {
+    
+    useEffect(() => {
+        const handler = (e) => e.preventDefault();
+        document.addEventListener("contextmenu", handler);
+      
+        return () => document.removeEventListener("contextmenu", handler);
+      }, []);
   return (
-    <div className=''>
-      <img src={studyInGermanyHeaderImg} alt="Study in Germany" />
+    <div className='cursor-none pointer-events-none'>
+      <img src={studyInGermanyHeaderImg} alt="Study in Germany" className='pointer-events-none select-none' />
     </div>
   )
 }
